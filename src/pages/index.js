7b530729f0aa14fa6849d1b2cc80c07a17d509a4
SuaferoanTJK <suarezroanfelipe@gmail.com>
2022-06-13 14:08:00 -0500

    pages info called from DatoCMS

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Layout from "../components/layout"
 import HotelImage from "../components/hotelImage"
+import IndexContent from "../components/indexContent"
 
 const index = () => {
   return (
     <>
       <Layout>
         <HotelImage />
+        <IndexContent />
       </Layout>
     </>
   )
